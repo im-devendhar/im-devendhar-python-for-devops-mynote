814dd37ff4f91bcf27a76faf4e8e08a865d9cf74
im-devendhar <devendharyadav781@gmail.com>
2025-11-22 20:14:02 +0530

    second commit - Python for DevOps mynote

---
+++ Day-5/cmdargs.py
@@ -0,0 +1,33 @@
+import sys
+
+def addition(a,b):
+    addition = a + b
+    return addition
+
+def subtraction(a,b):   # FIXED spelling
+    subtraction = a - b
+    return subtraction
+
+def multiplication(a,b):
+    multiplication = a * b
+    return multiplication
+
+a = float(sys.argv[1])
+operation = sys.argv[2]
+b = float(sys.argv[3])
+
+if operation == "addition":
+    output = addition(a,b)
+    print(output)
+
+elif operation == "subtraction":   # FIXED spelling
+    output = subtraction(a,b)
+    print(output)
+
+elif operation == "multiplication":
+    output = multiplication(a,b)
+    print(output)
+
+else:
+    print("unknown operation")
+
